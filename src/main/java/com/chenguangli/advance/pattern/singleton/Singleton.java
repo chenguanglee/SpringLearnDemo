7291b4b714894b4a0599ea59c3ea135fbc92b2b4
185109543@qq.com <185109543@qq.com>
2020-01-06 22:01:17 +0800

    add singleton

---
+++ src/main/java/com/chenguangli/advance/pattern/singleton/Singleton.java
@@ -0,0 +1,81 @@
+package com.chenguangli.advance.pattern.singleton;
+
+/**
+ * 单例模式
+ *
+ * @author chenguangli
+ */
+class SingletonLazy {
+
+    //懒汉模式(线程安全和不安全两种)
+    private static SingletonLazy instance = null;
+
+    private SingletonLazy() {
+    }
+
+    //线程安全的
+    public static synchronized SingletonLazy getInstanceSafe() {
+        if (instance == null) {
+            instance = new SingletonLazy();
+        }
+        return instance;
+    }
+
+    //双重校验锁
+    public static SingletonLazy getInstance() {
+        if (instance == null) {
+            synchronized (SingletonLazy.class) {
+                if (instance == null) {
+                    instance = new SingletonLazy();
+                }
+            }
+        }
+        return instance;
+    }
+
+    //线程不安全的
+    public static SingletonLazy getInstanceUnSafe() {
+        if (instance == null) {
+            instance = new SingletonLazy();
+        }
+        return instance;
+    }
+}
+
+class SingletonHungry {
+
+    //饿汉模式
+    private static SingletonHungry instance = new SingletonHungry();
+
+    private SingletonHungry() {
+    }
+
+    public static SingletonHungry getInstance() {
+        return instance;
+    }
+    //如何保证线程安全？由JVM类加载的特性，JVM在加载类的时候，是单线程的，所以可以保证只存在单一的实例，不需要加synchronized
+}
+
+class SingletonInnerClass {
+
+    //静态内部类
+    private static class SingletonHolder {
+        private static SingletonInnerClass instance = new SingletonInnerClass();
+    }
+
+    private SingletonInnerClass() {
+    }
+
+    public static SingletonInnerClass getInstance() {
+        return SingletonHolder.instance;
+    }
+
+}
+
+enum Signleton {
+    INSTANCE;
+}
+
+
+public class Singleton {
+}
---
+++ src/main/java/com/chenguangli/base/string/TestIntern.java
@@ -0,0 +1,40 @@
+package com.chenguangli.base.string;
+
+/**
+ * @author chenguangli
+ * @date 2020/1/5 12:07
+ */
+public class TestIntern {
+
+    public static void main(String[] args) {
+
+        //1.6版本 false
+        /**
+         * str2.intern()会把首次遇到的字符串实例复制到永久代中，返回的是永久代中实例的引用
+         * str2创建的字符串在堆上, 两者不相同
+         */
+
+        //1.7+版本 true
+        /**
+         * str2.intern() 不会复制实例，只是在常量池中记录首次出现的实例的引用
+         * 所以两者相同
+         */
+        String str2 = new StringBuilder("LEE").append("KING").toString();
+        System.out.println(str2.intern() == str2);
+
+        //1.6版本 false
+        /**
+         * 字符串 "java" 不属于首次出现, 所以str1.intern()返回的是在永久代的对象的引用
+         * str在堆上，所以返回false
+         */
+        //1.7+版本 false
+        /**
+         * str1.intern()返回的是常量池中记录首次出现在堆上的实例的引用
+         * str1重新new了, 所以false
+         */
+        String str1 = new StringBuilder("ja").append("va").toString();
+        System.out.println(str1.intern() == str1);
+
+
+    }
+}
